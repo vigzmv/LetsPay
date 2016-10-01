73857635cf22916d34cbe8ce8f1a6589d7ee05bd
Hiten Sharma <sharmahiten3@gmail.com>
2016-10-01 17:24:24 +0530

    models defined-1

--- LetsPay/settings.py
+++ LetsPay/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
     ]
 
 STATIC_ROOT = "/static/"
